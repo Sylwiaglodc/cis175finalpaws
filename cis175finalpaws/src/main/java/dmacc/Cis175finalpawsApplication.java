5df5e8feecc761eea6b15ff73210392ffe5763c5
Kevin Roney <karoney@dmacc.edu>
2023-03-23 19:17:09 -0500

    Initial

---
+++ cis175finalpaws/src/main/java/dmacc/Cis175finalpawsApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cis175finalpawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cis175finalpawsApplication.class, args);
+	}
+
+}
